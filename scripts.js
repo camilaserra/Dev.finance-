c0e5e093285f9157c1651117c9d247cd8c123e8c
Camila Serra <camilaserrasilva@gmail.com>
2021-02-03 21:06:23 -0300

    add.JS.transactions

---
+++ scripts.js
@@ -0,0 +1,64 @@
+const Modal = {
+    open() {
+        document
+        .querySelector('.modal-overlay')
+        .classList 
+        .add('active')
+    },
+    close() {
+        document
+        .querySelector('.modal-overlay')
+        .classList 
+        .remove('active')
+    }
+}
+
+const trasactions = [
+    {
+        id: 1,
+        descriptions: 'Luz',
+        amount: -50000,
+        date: '23/01/2021',
+    }, 
+
+    {
+        id: 2,
+        descriptions: 'Internet',
+        amount: 500000,
+        date: '23/01/2021',
+    }, 
+
+    {
+        id: 3,
+        descriptions: 'Criar Website',
+        amount: -20000,
+        date: '23/01/2021',
+    }, 
+]
+
+const trasaction = {
+    incomes() {
+        //somar as entradas
+    },
+    expenses() {
+        //somar as saídas
+    },
+    total() {
+        //entradas - saídas
+    }
+}
+
+ const DOM = {
+     innerHTMLTransaction() {
+            const html = `
+            <tr>
+                <td class="description">Luz</td>
+                <td class="expense">- R$ 500,00</td>
+                <td class="date">23/01/2021</td>
+                <td>
+                    <img src="./assets/minus.svg" alt="Remover transação">
+                </td>
+            </tr>
+        `
+     }
+ }
