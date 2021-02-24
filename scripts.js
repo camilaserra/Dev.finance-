365098d485721b45e22d2977ef36444dfd7b4281
Camila Serra <camilaserrasilva@gmail.com>
2021-02-24 19:19:29 -0300

    update.incomes.and,expenses

--- scripts.js
+++ scripts.js
@@ -14,54 +14,79 @@ const Modal = {
     }
 }
 
-const transactions = [
-    {
-        id: 1,
-        description: 'Luz',
-        amount: -50000,
-        date: '23/01/2021',
-    },
-
-    {
-        id: 2,
-        description: 'Internet',
-        amount: 500000,
-        date: '23/01/2021',
-    },
-
-    {
-        id: 3,
-        description: 'Criar Website',
-        amount: -20000,
-        date: '23/01/2021',
-    },
-    {
-        id: 4,
-        description: 'Contas do Mês',
-        amount: 20000,
-        date: '23/01/2021',
-    },
-]
-
 const Transaction = {
+    all: [
+        {
+            description: 'Luz',
+            amount: -50000,
+            date: '23/01/2021',
+        },
+    
+        {
+    
+            description: 'Internet',
+            amount: 500000,
+            date: '23/01/2021',
+        },
+    
+        {
+    
+            description: 'Criar Website',
+            amount: -20000,
+            date: '23/01/2021',
+        },
+        {
+    
+            description: 'APP',
+            amount: 20000,
+            date: '23/01/2021',
+        },
+    ],
+    add(transaction){
+        Transaction.all.push(transaction)
+
+        App.reload()
+    },
+
+    remove(index) {
+        Transaction.all.splice(index, 1)
+
+        App.reload()
+    },
+    
     incomes() {
         let income = 0;
-        Transaction.forEach(transaction => {
-            if( transaction.amount > 0 ) {
+        //pegar todas as transações
+        //para cada transação.
+        
+        Transaction.all.forEach(transaction => {
+           //somar ao uma variavel e return a variable
+            if (transaction.amount > 0 ) {
+                //somar a uma variavel e retornar a variavel
                 income += transaction.amount;
             }
         })
+        
         return income;
     },
-
+//em expenses, apenas troca-se o "income" por "expense", e o sinal "<" por ">"
     expenses() {
-        return "Aqui"
+        let expense = 0;
+              
+        Transaction.all.forEach(transaction => {
+            if (transaction.amount < 0 ) {
+            expense += transaction.amount;
+            }
+        })
+        
+        return expense;
     },
     total() {
-        return "Discover"
+        return Transaction.incomes() + Transaction.expenses();
     }
 }
 
+//substituir os dados do html com os dados do JS
 const DOM = {
 
     transactionsContainer: document.querySelector('#data-table tbody'),
@@ -75,34 +100,41 @@ const DOM = {
     },
   
     innerHTMLTransaction(transaction) {
-        const CSSclass = transaction.amount > 0 ? "income" : "expense";
+        const CSSclass = transaction.amount > 0 ? "income" : "expense"
 
         const amount = Utils.formatCurrency(transaction.amount)
+        
 
         const html = `
-           
+            
                 <td class="description">${transaction.description}</td>
                 <td class="${CSSclass}">${amount}</td>
                 <td class="date">${transaction.date}</td>
                 <td>
                     <img src="./assets/minus.svg" alt="Remover transação">
                 </td>
-        `
+            
+            `
         return html
+
     },
 
-    updtadeBalance () {
-        document.
-            getElementById('incomeDisplay')
-            .innerHTML = Transaction.incomes()
+    updateBalance () {
+        document
+            .getElementById('incomeDisplay')
+            .innerHTML = Utils.formatCurrency(Transaction.incomes())
 
-        document.
-            getElementById('expenseDisplay')
-            .innerHTML = Transaction.expenses()
+        document
+            .getElementById('expenseDisplay')
+            .innerHTML = Utils.formatCurrency(Transaction.expenses())
 
-        document.
-            getElementById('totalDisplay')
-            .innerHTML = Transaction.total()
+        document
+            .getElementById('totalDisplay')
+            .innerHTML = Utils.formatCurrency(Transaction.total())
+    },
+
+    clearTransactions() {
+        DOM.transactionsContainer.innerHTML = ""
     }
 
 }
@@ -110,8 +142,9 @@ const DOM = {
 const Utils = {
     formatCurrency(value) {
        const signal = Number(value) < 0 ? "-" : ""
+       
 
-       value = String(value).replace(/\D/g, "")
+       value = String(value).replace(/\D/g, "")  //expressão
 
        value = Number(value) / 100
 
@@ -125,10 +158,45 @@ const Utils = {
     }
 }
 
+const Form = {
+   
 
-transactions.forEach(function(transaction) {
-    DOM.addTransaction(transaction)
-})
+    submit(event) {
+        event.preventDefault()
+        //verificar se todas as informação foram preenchidas
+        
+        //formatar os dados para salvar
+        
+        //salvar
+        //apagar os dados do formulario
+        //modal feche
+        //atualizar a aplicação
+    }
+}
+
+const App = {
+    init() {
+
+        Transaction.all.forEach(transaction => {
+            DOM.addTransaction(transaction)
+        })
+        
+        DOM.updateBalance()
+        
+    },
+    reload() {
+        DOM.clearTransactions()
+        App.init()
+    },
+}
+
+App.init()
+
+//Transaction.remove()
 
 
-DOM.updtadeBalance()
+
+
+
+
+
